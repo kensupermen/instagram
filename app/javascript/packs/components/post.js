494f41f3ab0b65837e0a19f3bca5d5b0b9ee67ca
kensupermen <votantho93@gmail.com>
2017-11-27 14:49:19 +0700

    implement react

--- app/javascript/packs/components/post.js
+++ app/javascript/packs/components/post.js
@@ -1,16 +1,48 @@
 import * as React from "react";
+import axios from 'axios';
 
+const prefixURL = '/api/v1/'
+const domain = 'http://' + window.location.host
 class Post extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    let URL = prefixURL + 'posts/list';
+    axios.get(URL)
+      .then((response) => {
+        this.setState({
+          posts: response.data.posts
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div className="container">
         <div className="page">
           <div className="row">
-            <div className="post">
-              <div className="username">kensupermen</div>
-              <div className="image"></div>
-              <div className="caption"></div>
-            </div>
+            {
+              this.state.posts.map((post) => {
+                return (
+                  <div key={post.id} className="post" >
+                    <div className="username">kensupermen</div>
+                    <div className="image">
+                      <img src= { domain + post.image } alt="" />
+                    </div>
+                    <div className="caption">{post.caption}</div>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
       </div>
