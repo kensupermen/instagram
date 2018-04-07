ca5baa9e10399753f31e1a02fb5f0dbdbadd8f1c
kensupermen <votantho93@gmail.com>
2018-04-07 20:25:31 +0700

    refactor the fucking code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find params[:id]
     @images = []
-    @user.posts.map do |post|
+    @user.posts.each do |post|
       @images << post.image.url(:small)
     end
   end
